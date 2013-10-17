1aa02ecd6cf3f84261e2d8e57c0a4f1e9079a691
Kyle Buzsaki <saltor.tara@gmail.com>
2013-10-17 14:41:53 -0700

    Added a Factory to enable easier customizeability with the use.

--- src/Main.java
+++ src/Main.java
@@ -13,8 +13,6 @@ public class Main {
     public static void main(String[] args) {
         // previous method, relied on the static variable
         // WikiPage.target to provide the target
-        WikiPage wikiPage = WikiPage.getWikiPage("ocarina");
-        System.out.println(wikiPage.getPathToTarget());
         
         // new method, has you specify the target when you create the factory
         WikiPageFactory hitlerTargetedFactory = new WikiPageFactory("adolf_hitler");
@@ -25,9 +23,6 @@ public class Main {
         WikiPageFactory rwbyTargetedFactory = new WikiPageFactory("RWBY");
         WikiPage kittensPage = rwbyTargetedFactory.getWikiPage("kittens");
         System.out.println(kittensPage.getPathToTarget());
-        
-WikiPage wp1 = hitlerTargetedFactory.getWikiPage("kittens");
-WikiPage wp2 = hitlerTargetedFactory.getWikiPage("kittens");
         
     }
 
