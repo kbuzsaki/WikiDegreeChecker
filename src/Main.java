f981c43d8a48743983072f297ea22b396536ca29
kbuzsaki <saltor.tara@gmail.com>
2013-10-17 17:52:28 -0700

    Removed cruft from the main method and added comments outlining usage.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 
 import com.kbuzsaki.degreechecker.WikiPage;
 import com.kbuzsaki.degreechecker.WikiPageFactory;
+import java.util.List;
 
 
 
@@ -11,19 +12,20 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // previous method, relied on the static variable
-        // WikiPage.target to provide the target
+        // Usage Guidlines:
         
-        // new method, has you specify the target when you create the factory
+        // Create a factory with the title of the target page as an argument
         WikiPageFactory hitlerTargetedFactory = new WikiPageFactory("adolf_hitler");
+        
+        // Create a WikiPage with the title of the desired start page by
+        // using the static factory
         WikiPage ocarinaPage = hitlerTargetedFactory.getWikiPage("ocarina");
-        System.out.println(ocarinaPage.getPathToTarget());
         
-        // this lets you search for two separate targets
-        WikiPageFactory rwbyTargetedFactory = new WikiPageFactory("RWBY");
-        WikiPage kittensPage = rwbyTargetedFactory.getWikiPage("kittens");
-        System.out.println(kittensPage.getPathToTarget());
+        // Query the wiki page for its closest path to the target.
+        List<WikiPage> pathToTarget = ocarinaPage.getPathToTarget();
         
+        // Display the path
+        System.out.println(pathToTarget);
     }
 
 }
